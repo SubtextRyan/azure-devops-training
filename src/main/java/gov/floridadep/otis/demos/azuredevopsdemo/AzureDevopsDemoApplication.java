ca8e9dcedea0e8c1aef0c48becae439c81dfbe38
Ryan J. Johnson <ryan.j.johnson@dep.state.fl.us>
2019-09-26 10:07:34 -0400

    leprechauns!

---
+++ src/main/java/gov/floridadep/otis/demos/azuredevopsdemo/AzureDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package gov.floridadep.otis.demos.azuredevopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDevopsDemoApplication.class, args);
+	}
+
+}
